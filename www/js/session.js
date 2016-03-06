92fd68b26454ec31d2f4fc8f8cbd2715ac72c62a
nsellek <nick@seansellek.com>
2016-03-05 21:53:56 -0500

    initial commit

---
+++ www/js/session.js
@@ -0,0 +1,29 @@
+var Peek = Peek || {};
+
+Peek.Session = (function () {
+  var instance;
+  function init() {
+    var sessionIdKey = "Peek-session";
+    return {
+      // Public methods and variables.
+      set: function (sessionData) {
+        window.localStorage.setItem(sessionIdKey, JSON.stringify(sessionData));
+      },
+      get: function () {
+        var result = null;
+        try {
+          result = JSON.parse(window.localStorage.getItem(sessionIdKey));
+        } catch(e){}
+          return result;
+      }
+    };
+  };
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+}());
